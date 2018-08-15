6711defccfc5483b5ceda0eca82c3d14f08f1fa5
Ashley Corah <Ashley.Corah@skyscanner.net>
2018-08-15 22:43:03 +0100

    Add body parser

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const application = require('./routes/application');
 
 // App
 const app = express();
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true,
+}));
 
 app.use('/application', application);
 
 app.listen(1200, '0.0.0.0');
 
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on http://1200:0.0.0.0`);
